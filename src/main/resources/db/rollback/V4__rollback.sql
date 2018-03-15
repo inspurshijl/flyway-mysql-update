508dadefebfcdca5dcec1ac80cc4b68239246e45
shijialiang <64207100@qq.com>
2018-03-15 19:07:44 +0800

    增加回滚

--- src/main/resources/db/rollback/V4__rollback.sql
+++ src/main/resources/db/rollback/V4__rollback.sql
@@ -1,3 +1,11 @@
-DROP TABLE IF
-EXISTS dept;
+DROP TABLE IF EXISTS dept;
 DROP TABLE IF EXISTS work_space;
+CREATE TABLE app
+(
+Id int NOT NULL,
+LastName varchar(255) NOT NULL,
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255),
+PRIMARY KEY (Id)
+);
